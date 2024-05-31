2dcfb1b6cbdf039206cf0611d61eb1e1f46e9954
Niklas Koponen <ext.niklas.koponen@varma.fi>
2024-05-31 18:49:02 +0300

    Added conjure and mkdnflow.

--- init.lua
+++ init.lua
@@ -863,6 +863,17 @@ require('lazy').setup({
       --    - Treesitter + textobjects: https://github.com/nvim-treesitter/nvim-treesitter-textobjects
     end,
   },
+  {
+    'Olical/conjure',
+  },
+  {
+    'jakewvincent/mkdnflow.nvim',
+    config = function()
+      require('mkdnflow').setup {
+        -- Config goes here; leave blank for defaults
+      }
+    end,
+  },
 
   -- The following two comments only work if you have downloaded the kickstart repo, not just copy pasted the
   -- init.lua. If you want these files, they are in the repository, so you can just download them and
